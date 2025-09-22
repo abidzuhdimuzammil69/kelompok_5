390040f9e9cea4af9d89784b6ebfa8cb05f54b3d
unknown <abidzuhdimuzammil69@gmail.com>
2025-09-22 07:15:03 +0700

    Code Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,13 @@
+public class Dosen {
+    String namaDosen;
+    int nidnDosen;
+
+    void mengajarMataKuliah() {
+        System.out.println("PBO");
+    }
+
+    public String mengerjakanPenelitian(){
+        return "dosen mengerjakan penelitian";
+    }
+    
+}
